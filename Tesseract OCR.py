19663c083792b4c637c1d846e60d2703c2ee43bf
mwolak07 <mwolak07@gmail.com>
2019-04-18 17:33:07 -0400

    added filtering

--- Tesseract OCR.py
+++ Tesseract OCR.py
@@ -4,12 +4,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
+# Image read from file system and converted to numpy array
 image = cv2.imread('images/Tesseract-Test.png', flags=cv2.IMREAD_GRAYSCALE)
 image = np.array(image)
 
+# Displaying the original image
 plt.imshow(image, cmap='gray')
+plt.title("Original")
 plt.show()
 
+# Filtering the image with openCV medianBlur function with 5x5 window to minimize noise
+image = cv2.medianBlur(image, 3)
+
+# Displaying filtered image
+plt.imshow(image, cmap='gray')
+plt.title("Filtered")
+plt.show()
+
+# Using tesseract OCR to extract text from the image
 text = image_to_string(Image.fromarray(image))
 
-print("Text: " + text)
+# Showing the extracted text
+print("Text: \n" + text)
